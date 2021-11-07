7ccb779c49fc7befa0ffd169a88fdbe3fda8543f
Martin <mail@mvb1.de>
2021-11-07 16:54:48 +0100

    version tag update

--- wp_wpcat_json_rest.php
+++ wp_wpcat_json_rest.php
@@ -9,7 +9,7 @@
  * Plugin Name:       Ext_REST_Media_Lib
  * Plugin URI:        https://github.com/MartinvonBerg/Ext_REST_Media_Lib
  * Description:       Extend the WP-REST-API to work with Wordpress Media-Library directly. Add and Update images even to folders. Only with Authorization.
- * Version:           0.0.15
+ * Version:           0.0.16
  * Author:            Martin von Berg
  * Author URI:        www.mvb1.de
  * License:           GPL-2.0
